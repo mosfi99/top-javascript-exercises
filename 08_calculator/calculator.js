a399e851ad208c6d353d4239487ef4f9f1e70205
mosfi99 <mosfi99.mosfi99@gmail.com>
2025-05-08 17:37:54 +0200

    Add my solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,50 @@
-const add = function() {
-	
+const add = function (...numbers) {
+	return numbers.reduce((total, num) => total + num, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function (...numbers) {
+	return numbers.reduce((total, num) => {
+		total === 0 ? (total += num) : (total -= num);
+		return total;
+	}, 0);
 };
 
-const sum = function() {
-	
+// same as add
+const sum = function (numbers) {
+	return numbers.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+	return numbers.reduce((total, num) => (total *= num), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+	let factorial = n;
+	if (factorial === 0 || factorial === 1) return 1;
+	// if n is 5
+	let i = n - 1; // 4
+	while (i >= 1) {
+		// 4 >= 1, there are 4 iterations, i = 4, 3, 2, 1
+		factorial *= i;
+		// 5 * 4 = 20
+		// 20 * 3 = 60
+		// 60 * 2 = 120
+		// 120 * 1 = 120
+		--i;
+	}
+	return factorial;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
