23980072060396ec09399f96f7ec3d9032b7c3e6
mosfi99 <mosfi99.mosfi99@gmail.com>
2025-05-09 14:27:11 +0200

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	// Since we only consider letters and numbers, create a variable containing all valid characters
+	const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+	const cleanString = str
+		.toLowerCase()
+		.split('')
+		.filter((character) => alphanumerical.includes(character))
+		.join('');
+
+	const reverseString = cleanString.split('').reverse().join('');
+
+	return cleanString === reverseString;
 };
 
 // Do not edit below this line
